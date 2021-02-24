5a70b17c10114ed654893c14f0843d96c7414229
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2021-02-24 13:19:53 -0500

    Image and text now display in adapter

--- app/src/main/java/edu/temple/imagepicker/GalaxyAdapter.java
+++ app/src/main/java/edu/temple/imagepicker/GalaxyAdapter.java
@@ -2,10 +2,15 @@ package edu.temple.imagepicker;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -39,13 +44,32 @@ public class GalaxyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(context);
+        TextView textView;
+        ImageView imageView;
+
+        LinearLayout linearLayout;
+
+        if(convertView == null){
+            linearLayout = new LinearLayout(context);
+            textView = new TextView(context);
+            imageView = new ImageView(context);
+            textView.setTextSize(20);
+            textView.setPadding(10,10,10,10);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.addView(imageView);
+            linearLayout.addView(textView);
+            imageView.getLayoutParams().height = 150;
+            imageView.getLayoutParams().width = 150;
+        } else {
+            linearLayout = (LinearLayout) convertView;
+            imageView = (ImageView) linearLayout.getChildAt(0);
+            textView = (TextView) linearLayout.getChildAt(1);
+        }
+
+        imageView.setImageResource(imageRes[position]);
         textView.setText(items.get(position));
 
-        textView.setPadding(10,8,8,8);
-        textView.setBackgroundColor(Color.MAGENTA);
-
-        return textView;
+        return linearLayout;
     }
 
     @Override
